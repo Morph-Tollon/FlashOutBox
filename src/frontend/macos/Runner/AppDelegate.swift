0f08f7c2718291b105542d0e16f5473f14498007
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-05-10 17:13:31 +0100

    feat: Add OpenGithubIssues action to AppDelegate for quick access to issue tracker

--- src/frontend/macos/Runner/AppDelegate.swift
+++ src/frontend/macos/Runner/AppDelegate.swift
@@ -3,11 +3,21 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
-
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-    return true
-  }
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+    
+    @IBAction func OpenGithubIssues(_ sender: Any){
+        if let url = URL(string: "https://github.com/Morph-Tollon/FlashOutBox/issues") {
+            NSWorkspace.shared.open(url)
+            
+            
+        }
+        
+    }
+    
 }
