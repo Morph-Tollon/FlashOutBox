0165a4e53a359892f2ffb0689f48350890c75392
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-05-12 10:25:42 +0100

    feat: Initialize navigation channel in AppDelegate for improved method invocation

--- src/frontend/macos/Runner/AppDelegate.swift
+++ src/frontend/macos/Runner/AppDelegate.swift
@@ -3,12 +3,25 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
+    var navigationChannel: FlutterMethodChannel?
+
     override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
     
     override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
+    }
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        // Grab the controller from the main window
+        if let window = NSApplication.shared.windows.first,
+           let controller = window.contentViewController as? FlutterViewController {
+            
+            navigationChannel = FlutterMethodChannel(
+                name: "uk.co.morphtollon.flashoutbox/channel",
+                binaryMessenger: controller.engine.binaryMessenger
+            )
+        }
     }
     
     @IBAction func OpenGithubIssues(_ sender: Any){
@@ -17,7 +30,15 @@ class AppDelegate: FlutterAppDelegate {
             
             
         }
-        
     }
+        
+    @IBAction func settingsPage(_ sender: Any){
+        navigationChannel?.invokeMethod("page", arguments:"/settings")
+
+
+    }
+        
+    
     
 }
+
